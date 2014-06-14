f64ae0cb2585a7c2cd95f96edf90fb4355d9871b
Stuart Wilson <stuart@stuartwilsondev.com>
2014-06-14 19:39:02 +0100

    Generate CoreBundle

---
+++ src/Yeargle/Bundle/CoreBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Yeargle\Bundle\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('YeargleCoreBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
