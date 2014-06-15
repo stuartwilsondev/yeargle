446e3d7acf7c1899cdf1047d192897f2da931cd8
Stuart Wilson <stuart@stuartwilsondev.com>
2014-06-15 10:36:35 +0100

    Added favicon. Started making Symfony from for Main search form

--- src/Yeargle/Bundle/CoreBundle/Controller/DefaultController.php
+++ src/Yeargle/Bundle/CoreBundle/Controller/DefaultController.php
@@ -3,11 +3,27 @@
 namespace Yeargle\Bundle\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Yeargle\Bundle\CoreBundle\Resources\form\type\MainSearch;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('YeargleCoreBundle:Default:index.html.twig');
+        $searchForm = $this->createForm(
+            new MainSearch()
+        );
+
+        $searchForm->handleRequest($request);
+
+        if ($searchForm->isValid()) {
+
+        }
+
+        return $this->render('YeargleCoreBundle:Default:index.html.twig', array(
+            'searchForm' => $searchForm->createView()
+        ));
+
+
     }
 }
---
+++ src/Yeargle/Bundle/CoreBundle/Resources/form/type/MainSearch.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stuartwilson
+ * Date: 14/06/2014
+ * Time: 21:27
+ */
+
+namespace Yeargle\Bundle\CoreBundle\Resources\form\type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+
+class MainSearch extends AbstractType
+{
+
+    protected $formName = 'Yeagle_Search';
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return $this->formName;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+//type="text" id="search-query" name="searchquery" autocomplete="off" placeholder="Search Within The Last Year"
+        $builder
+            ->add('search_element_text', 'text', array(
+                'attr' => array(
+                    'id'=>'search-query',
+                    'name' => 'searchquery',
+                    'autocomplete' => 'off',
+                    'placeholder' => 'Search Within The Last Year'
+                ),
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('min' => 3)),
+                )
+            ))
+            ->add('search_element_search', 'submit', array(
+                'attr' => array('class'=> 'button'),
+            ))
+            ->add('search_element_random', 'submit', array(
+                'attr' => array('class'=> 'button'),
+            ));
+
+                // <input type="button" class="button" value="Search"/>
+                        //<input type="button" class="button" value="Show Me Something Random"/>
+    }
+
+}
